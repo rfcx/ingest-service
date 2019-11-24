4c8943edc0c11b1f811d2e20e1a20fc36b4d5b93
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2019-11-24 18:27:18 +0300

    remove functions_v2

--- functions_v2/index.js
+++
@@ -1,8 +0,0 @@
-// const functions = require('firebase-functions');
-// const storage = require('./services/storage')
-
-// HTTP endpoints
-// exports.api = functions.https.onRequest(require('./api'));
-
-// Background triggers
-// exports.ingest = functions.storage.bucket(storage.bucketName).object().onFinalize(require('./triggers/ingest'))
