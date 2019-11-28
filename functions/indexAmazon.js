f6705230f40421b6660447e36a62da85c947bb80
Antony Harfield <antonyharfield@gmail.com>
2019-11-28 20:07:32 +0700

    WB-532 Fix refactor error on amazon start

--- functions/indexAmazon.js
+++ functions/indexAmazon.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const api = require('./api')
 
 const port = process.env.PORT || 3030
-app.listen(port, function () {
+api.listen(port, function () {
   console.log(`App is listening on port ${port}`)
 })
