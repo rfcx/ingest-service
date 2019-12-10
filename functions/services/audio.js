ee379c4944343465cb2fe8aa52540552ce3da412
Antony Harfield <antonyharfield@gmail.com>
2019-12-11 01:54:25 +0700

    WB-370 Initial code for splitting files using ffmpeg-fluent

--- functions/services/audio.js
+++ functions/services/audio.js
@@ -1,5 +1,6 @@
 const probe = require('ffmpeg-probe')
 const fs = require('fs')
+const ffmpeg = require('fluent-ffmpeg')
 
 /* Run sox (or in this case ffmpeg) to get the sample rate and duration
 ** results looks like:
@@ -22,15 +23,50 @@ function identify (filePath) {
 }
 
 /**
- *
+ * Split an audio file into segments of a maximum duration
+ * - if the source file is longer than the maximum duration then it will be cut into segments of maximum duration in length
+ * - the last output file may be shorter than the maximum duration
  * @param {String} sourceFile - path to source file on disk
- * @returns {Object[]} splittedFiles - array with objects with segments information (local path, duration, start, end timestamps)
+ * @param {String} destinationPath - path (directory) to output the file segments
+ * @param {Number} maxDuration - the maximum duration of a segment
+ * @returns {Object[]} - array with objects with segments information (local path, duration)
  */
 function split (sourceFile, destinationPath, maxDuration) {
-  console.log('split file', sourceFile)
-  const destinationFile = destinationPath + '/' + sourceFile
-  fs.copyFileSync(sourceFile, destinationFile)
-  return Promise.resolve([{ path: destinationFile, duration: maxDuration }])
+  destinationPath += destinationPath.endsWith('/') ? '' : '/'
+  const outputFileFormat = destinationPath + sourceFile.replace(/\.([^.]*)$/, '.%03d.$1') // convert hello.wav to hello.%03d.wav
+
+  return new Promise((resolve, reject) => {
+    const command = ffmpeg(sourceFile)
+      .noVideo()
+      .audioCodec('copy')
+      .output(outputFileFormat)
+      .outputOptions([
+        '-f segment', // output as file segments
+        `-segment_time ${maxDuration}`, // split into X sec segments
+        // '-segment_frames 0,24000,48000' // split into segments by frame boundaries - maybe useful in future
+        '-segment_list pipe:1' // output a list of the created segments to stdout
+      ]).on('start', function (commandLine) {
+        console.log('Spawned Ffmpeg with command: ' + commandLine)
+      }).on('progress', function (progress) {
+        console.log('Processing: ' + progress.percent + '% done')
+      })
+
+    const timeout = setTimeout(function () {
+      command.kill()
+      reject(Error('Timeout')) // TODO: move to errors
+    }, 60000)
+
+    command.on('error', function (err, stdout, stderr) {
+      clearTimeout(timeout)
+      reject(err)
+    }).on('end', function (stdout, stderr) {
+      clearTimeout(timeout)
+      const outputFiles = stdout.trim().split('\n').map(x => {
+        return { path: x, duration: maxDuration }
+      })
+      resolve(outputFiles)
+    }).run()
+  })
 }
 
 module.exports = {
