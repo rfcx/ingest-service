768df7a23c7234fa9374c3485f682db40bc3a8b9
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-08-15 01:17:10 +0300

    upgrade mongoose version; update mongoose connection config to support keepAlive

--- functions/utils/mongo.js
+++ functions/utils/mongo.js
@@ -8,11 +8,15 @@ db.once('open', function() {
   console.log('Connected to MongoDB');
 });
 
+console.log('Establishing connection to MongoDB')
 mongoose.connect(mongoUri, {
   user: process.env.MONGO_USERNAME,
   pass: process.env.MONGO_PASSWORD,
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  keepAlive: true,
+  keepAliveInitialDelay: 300000,
+  serverSelectionTimeoutMS: 30000
 });
 
 module.exports = db;
