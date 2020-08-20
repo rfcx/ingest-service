fc696924fe28e3d4d0a70d3a596262a9516112ea
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-08-20 14:13:11 +0300

    add ttl to upload model

--- functions/services/db/models/mongoose/upload.js
+++ functions/services/db/models/mongoose/upload.js
@@ -6,7 +6,7 @@ const UploadSchema = new mongoose.Schema({
   streamId: String,
   userId: String,
   status: Number,
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now, expires: '30d' }, // expires in 30 days
   updatedAt: { type: Date, default: Date.now },
   timestamp: Date,
   originalFilename: String,
