c603039a9cd263b89fdf229ad1b7741157be0ab1
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-08-20 14:26:31 +0300

    remove unused stream schema and methods

--- functions/services/db/mongo.js
+++ functions/services/db/mongo.js
@@ -1,6 +1,5 @@
 const db = require('../../utils/mongo');
 const UploadModel = require('./models/mongoose/upload').Upload;
-const StreamModel = require('./models/mongoose/stream').Stream;
 const moment = require('moment-timezone');
 const hash = require('../../utils/hash');
 
@@ -56,50 +55,9 @@ function updateUploadStatus (uploadId, statusNumber, failureMessage = null) {
     })
 }
 
-// The following functions for Streams are not used for ingestStream script at the current time. They are here just for consistency with `google` approach.
-
-function createStream (name) {
-  const token = '1234' // TODO: this is only here for legacy calls to checkin api
-  const streamGuid = hash.randomString(12);
-  let stream = new StreamModel({
-    guid: streamGuid,
-    token,
-    name
-  })
-
-  return stream.save()
-    .then(() => {
-      return { id: streamGuid, token };
-    });
-}
-
-function getStream (guid) {
-  return StreamModel.find({ guid });
-}
-
-function editStream (guid, name, site) {
-  return getStream(guid)
-    .then((stream) => {
-      stream.name = name
-      if (site !== undefined) {
-        stream.site = site
-      }
-      return stream.save();
-    })
-}
-
-function deleteStream (guid) {
-  return StreamModel.deleteOne({ guid });
-}
-
-
 module.exports = {
   generateUpload,
   getUpload,
   updateUploadStatus,
-  status,
-  createStream,
-  getStream,
-  editStream,
-  deleteStream,
+  status
 }
