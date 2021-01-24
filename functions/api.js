a406a0dfed9e4960e5a51074877e96ae52066649
Antony Harfield <antonyharfield@gmail.com>
2021-01-24 15:54:08 +0700

    PI-254 Refactor logging

--- functions/api.js
+++ functions/api.js
@@ -11,30 +11,9 @@ if (process.env.NODE_ENV === 'development' && process.env.PLATFORM === 'google')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: '1mb' }))
+
 if (process.env.PLATFORM === 'amazon') {
-  const winston = require('winston')
-  const expressWinston = require('express-winston')
-  app.use(expressWinston.logger({
-    transports: [
-      new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-      winston.format.json()
-    ),
-    meta: true,
-    requestWhitelist: ['headers', 'body'],
-    headerBlacklist: [
-      'host', 'x-request-id', 'x-real-ip', 'x-forwarded-for', 'x-forwarded-host', 'x-forwarded-port', ' x-forwarded-proto',
-      'x-original-uri', 'x-scheme', 'x-original-forwarded-for', 'content-length', 'accept', 'connection', 'origin', 'sec-fetch-mode',
-      'sec-fetch-site', 'referer', 'accept-encoding', 'accept-language', 'user-agent'],
-    expressFormat: true,
-    ignoreRoute: function (req, res) {
-      if (req.method === 'GET' && (/\/(uploads\/.+|health-check|metrics)/).test(req.url)) {
-        return true
-      }
-      return false
-    }
-  }))
+  app.use(require('./services/logging/amazon'))
 }
 
 app.use('/docs', require('./docs'))
