967ca53f9d2ceb0a740073087127c7e6fe7f68c5
TooSeriuz <frongs@rfcx.org>
2024-01-26 16:45:37 +0700

    Deny specific file size each extension in consumer. Deny audio with >1hr duration

--- services/consumer/amazon.js
+++ services/consumer/amazon.js
@@ -3,6 +3,10 @@ const AWS = require('../../utils/aws')
 const { ingest } = require('../rfcx/ingest')
 const { parseUploadFromFileName } = require('./misc')
 const TimeTracker = require('../../utils/time-tracker')
+const db = require('../db/mongo')
+
+const flacLimitSize = 200_000_000
+const wavLimitSize = 200_000_000
 
 const consumer = Consumer.create({
   queueUrl: process.env.SQS_INGEST_TRIGGER_QUEUE_URL,
@@ -13,7 +17,14 @@ const consumer = Consumer.create({
     for (const file of files) {
       const { fileLocalPath, streamId, uploadId } = parseUploadFromFileName(file.key)
       try {
-        await ingest(file.key, fileLocalPath, streamId, uploadId)
+        const fileExtension = file.bucket.name.split('.').pop().toLowerCase()
+        if (fileExtension.toLowerCase() === 'flac' && file.size > flacLimitSize) {
+          db.updateUploadStatus(uploadId, db.status.FAILED, 'This flac file size is exceeding our limit (200MB)')
+        } else if (fileExtension.toLowerCase() === 'wav' && file.size > wavLimitSize) {
+          db.updateUploadStatus(uploadId, db.status.FAILED, 'This wav file size is exceeding our limit (200MB)')
+        } else {
+          await ingest(file.key, fileLocalPath, streamId, uploadId)
+        }
       } catch (e) {
         return false
       }
